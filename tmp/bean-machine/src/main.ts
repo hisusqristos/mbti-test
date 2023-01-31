f77a8df4c6d6ad7ecf131db006df9b207e73a5e5
antaranian <mushegh.antaranyan@gmail.com>
2023-01-31 11:40:00 +0400

    Implement bean machine
    Integrate interactive canvas into the app

--- tmp/bean-machine/src/main.ts
+++ tmp/bean-machine/src/main.ts
@@ -1,31 +1,134 @@
 import "./style.css";
 
-import { Bodies, Composite, Engine, Render, Runner } from "matter-js";
-
-// module aliases
+import {
+  Bodies,
+  Common,
+  Composite,
+  Composites,
+  Engine,
+  Render,
+  Runner,
+  Mouse,
+  MouseConstraint,
+} from "matter-js";
 
 // create an engine
-var engine = Engine.create();
-
-// create a renderer
-var render = Render.create({
-  element: document.body,
-  engine: engine,
+var engine = Engine.create({
+  gravity: { x: 0, y: 0.35 },
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+// create a renderer
+const container = document.getElementById("app");
+if (container == null) {
+  throw new Error("No wrapper element found");
+}
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+const render = Render.create({
+  element: container,
+  engine: engine,
+  options: {
+    width: 960,
+    height: 720,
+    // showCollisions: true,
+    wireframes: false,
+    showAngleIndicator: false,
+    background: "transparent",
+  },
+});
+
+// main attributes
+const width = render.bounds.max.x;
+const height = render.bounds.max.y;
+const radius = 6;
+const n = 16;
+const gap = (width - (n + 1) * (2 * radius)) / n;
+const bucketHeight = 96;
+
+// add bodies
+
+const drop = Bodies.circle(width / 2, 20, radius * 2, {
+  friction: 0.00001,
+  restitution: 0.6,
+  density: 0.002,
+  render: {
+    fillStyle: "#B48EAD",
+    strokeStyle: "#EBCB8B",
+    lineWidth: 2,
+  },
+});
+Composite.add(engine.world, drop);
+
+const bucketWallAt = (x: number, y: number) => {
+  return Bodies.rectangle(x, y, radius * 2, bucketHeight, {
+    isStatic: true,
+    render: {
+      fillStyle: "#2E3440",
+      lineWidth: 0,
+    },
+  });
+};
+const buckets = Composites.stack(
+  0,
+  height - bucketHeight,
+  n + 1,
+  1,
+  gap,
+  0,
+  bucketWallAt
+);
+Composite.add(engine.world, buckets);
+
+const ground = Bodies.rectangle(width / 2, height - radius, width, 2 * radius, {
+  isStatic: true,
+  restitution: 0,
+  frictionStatic: 0.8,
+  render: {
+    fillStyle: "#2E3440",
+    lineWidth: 0,
+  },
+});
+Composite.add(engine.world, ground);
+
+const pinAt = (x: number, y: number, col: number, row: number) => {
+  const offset = () => {
+    // offset only the first column
+    if (col > 0) return 0;
+    // offset every other row
+    return row % 2 === 0 ? gap / 2 + 2 * radius : radius;
+  };
+  return Bodies.polygon(x + offset(), y, 5, radius, {
+    angle: Common.random(0, 2 * Math.PI),
+    restitution: 0.5,
+    frictionStatic: 0.1,
+    isStatic: true,
+    render: {
+      fillStyle: "#2E3440",
+      strokeStyle: "#3B4252",
+      lineWidth: 2,
+    },
+  });
+};
+const pins = Composites.stack(0, 108, 17, 9, gap, gap, pinAt);
+Composite.add(engine.world, pins);
+
+// add mouse control
+const mouse = Mouse.create(render.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+    render: {
+      visible: false,
+    },
+  },
+});
+Composite.add(engine.world, mouseConstraint);
 
 // run the renderer
 Render.run(render);
 
 // create runner
-var runner = Runner.create();
+const runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
